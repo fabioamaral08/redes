0b26a420f903af9040798125c2c2d6365061def5
fabio <fabio@186.217.108.170>
2017-07-25 17:43:30 -0300

    Jogador C Peao Inicio da Aplicacao

---
+++ src/tpredes/Aplicacao.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpredes;
+
+import java.net.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author fabio
+ */
+public class Aplicacao implements Runnable{
+    private ServerSocket ss;
+    private DatagramSocket socUDP;
+    private int portaUDP;
+    private int portaTCP;
+    private ArrayList<JogadorC> jogadores;
+    private int num;
+    private String ipServer;
+    private int portaServer;
+
+    @Override
+    public void run() {
+        
+    }
+    
+    
+}
