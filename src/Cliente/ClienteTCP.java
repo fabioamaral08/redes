46d0544f950c3ffbef6f073e07ded3a728962f5d
Gustavoo <Gustavoo@186.217.108.119>
2017-08-02 11:55:54 -0300

    Cliente TCP switch começado.

--- src/Cliente/ClienteTCP.java
+++ src/Cliente/ClienteTCP.java
@@ -7,6 +7,7 @@ package Cliente;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -32,6 +33,40 @@ public class ClienteTCP implements Runnable{
     @Override
     public void run() {
        //switch
+    }
+    
+    public void trataMensagem(String mensagem){
+        StringTokenizer tk = new StringTokenizer(mensagem, " ");
+        String msgPartida = "";
+        int aux1, aux2;
+        
+        switch(tk.nextToken()){
+            case "040": //Atualiza dados
+                aux1 = Integer.valueOf(tk.nextToken()); //valor dado 1
+                aux2 = Integer.valueOf(tk.nextToken()); //valor dado 2
+                //Partida.atualizaDados(aux1, aux2);   //Método que atualiza dados na aplicação
+                break;
+                
+            case "041": //Novo saldo
+                aux1 = Integer.valueOf(tk.nextToken()); //valor saldo
+                //Partida.atualizaSaldo(aux1);              //Método pra atualizar saldo
+                break;
+             
+            case "042":
+                aux1 = Integer.valueOf(tk.nextToken()); //número da casa da propriedade
+                //Partida.novoDono(aux1);
+                break;
+                
+            case "043":
+                aux1 = Integer.valueOf(tk.nextToken()); //número da casa da propriedade
+                //Partida.constroi(aux1);
+                break;
+                
+            case "044":
+                aux1 = Integer.valueOf(tk.nextToken()); //número da carta
+                //Partida.sorteOuReves(aux1);
+                break;
+        }
     }
     
 }
